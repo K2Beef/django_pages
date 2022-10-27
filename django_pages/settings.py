8fa227b4d9fda25357a5fb8379cb742204dfd7fb
Keith Aldrich <kaldri4@lsu.edu>
2022-10-27 17:44:12 -0500

    second commit

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y==a(xpbyhi(c1p1k(63hjc1!s*qbj1ty%i!(1bsj@ih(l&3@e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
